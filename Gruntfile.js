689c113eeae1d96d0ceff8f38428c2680a893fa9
Chris Burgess <chris@giantrobot.co.nz>
2017-03-12 09:17:36 +1300

    Add grunt bump.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-release');
+  grunt.loadNpmTasks('grunt-bump');
 
   grunt.initConfig({
     mochaTest: {
